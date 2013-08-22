ed50fc994902b82a6ea79ba1d1636c65897f8ed6
Alex Phinizy <AlexPhinizy@gmail.com>
2013-08-22 15:42:41 -0400

    phase 5 done

--- echo.rb
+++ echo.rb
@@ -2,6 +2,7 @@ def playback (say)
   return "You said: #{say}"
 end
 
+
 puts "What do you want to say"
 
 input = gets.chomp
@@ -9,6 +10,17 @@ input = gets.chomp
 # if i enter nothing it should output "Ok, fine"
 if input == "Nothing!"
   puts "Ok, fine!"
+elsif input == "I have something prepared"
+  puts "Ok, where can I find what you want to say?"
+  filename=gets.chomp
+  puts "Loading #{filename}"
+  speech = []
+  File.open(filename, "r").each_line {|line| speech << line}
+
+  puts playback speech[0]
+  speech[1..-2].each {|x| puts "Then, you said: #{x}"}
+  puts "Finally you said: #{speech[-1]}"
+  puts "Phew! Glad you got all #{speech.length} of those things off your chest!"
 #if I enter I have a lot to say it should output 'i don't have time for that right now'
 elsif input == "I have a lot to say"
   puts "Ok, let's hear it!"
