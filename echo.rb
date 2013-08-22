a429b539a0dc321a34927a349d670a0cb0206b32
Alex Phinizy <AlexPhinizy@gmail.com>
2013-08-22 14:14:28 -0400

    phase ii

--- echo.rb
+++ echo.rb
@@ -2,10 +2,16 @@ def playback (say)
   return "You said: #{say}"
 end
 
-
 puts "What do you want to say"
 
 input = gets.chomp
 
-
-puts playback(input)
+# if i enter nothing it should output "Ok, fine"
+if input == "Nothing!"
+  puts "Ok, fine!"
+#if I enter I have a lot to say it should output 'i don't have time for that right now'
+elsif input == "I have a lot to say"
+  puts "I don't have time for that right now"
+else
+  puts playback(input)
+end
