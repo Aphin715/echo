d833d9d6ebd4d72f858d66dabe8a2c4ced67c97f
Alex Phinizy <AlexPhinizy@gmail.com>
2013-08-22 14:39:32 -0400

    echo phase III

--- echo.rb
+++ echo.rb
@@ -11,7 +11,16 @@ if input == "Nothing!"
   puts "Ok, fine!"
 #if I enter I have a lot to say it should output 'i don't have time for that right now'
 elsif input == "I have a lot to say"
-  puts "I don't have time for that right now"
+  puts "Ok, let's hear it!"
+  sayings = []
+  while (input=gets.chomp) != "done"
+    sayings << input
+  end
+  puts playback sayings[0]
+  sayings[1..-2].each {|x| puts "Then, you said: #{x}"}
+  puts "Finally you said: #{sayings[-1]}"
+  puts "Phew! Glad you got all #{sayings.length} of those things off your chest!"
 else
   puts playback(input)
 end
+
